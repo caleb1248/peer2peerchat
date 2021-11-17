c476b9bc10438fda9275c9a54d1790f147e48a41
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2021-11-17 14:06:18 -0500

    epic commiting moment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var server = http.createServer((req, res) => {
-    res.addHeader("ContentType")
+    res.addHeader("Content-Type: text/html");
+    fs.createReadStream("index.html").pipe(res);
+    res.end();
 })
