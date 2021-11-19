26e9e8a4bd8a3d3bd1d797bb0ebd8ad2eb162a99
Caleb Liu <94559723+caleb1248@users.noreply.github.com>
2021-11-19 12:50:03 +0000

    draft

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ var webserver = require("express")();
 webserver.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 	res.end();
-})
+});
+webserver.get("/n.js",(req,res) => {
+	res.sendFile(__dirname + "/n.js");
+	res.end();
+});
 var ws = require("ws");
-var WebSocketServer = ws.Server;
-var wsserver = new WebSocketServer(
+var wsserver = new ws.Server(
 	{
-		server : webserver
+		server : webserver,
+		port : 8080
 	}
 );
+wsserver.on("connection",socket => {
+	console.log("connection");
+	socket.on("message",(data) => {
+		console.log(data);
+	})
+});
+webserver.up
+webserver.listen(80);
