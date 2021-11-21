d6af967458a038b0161e20cd9270dd7d23c8eace
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2021-11-21 15:40:20 -0500

    still a draft...

--- n.js
+++ n.js
@@ -1,7 +1,10 @@
-var connection = new RTCPeerConnection();
-var channel = connection.createDataChannel("sus");
-navigator.mediaDevices.getUserMedia({video:true}).then(m => {
-    m.getVideoTracks().forEach(track => {
-        connection.addTrack(track);
-    })
-})
+var pc = new RTCPeerConnection({
+    iceServers : [{
+        urls : "stun:stun.l.google.com:17569"
+    }]
+});
+pc.addEventListener("icecandidate",function(e){
+    if(e.candidate){
+        s
+    }
+}
