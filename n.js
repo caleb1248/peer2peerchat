ef3bb2f94c08f999c094a3ee88cbf86a4c26d136
Caleb Liu <94559723+caleb1248@users.noreply.github.com>
2021-11-23 21:20:28 +0000

    draft

--- n.js
+++ n.js
@@ -1,10 +1,22 @@
-var pc = new RTCPeerConnection({
-    iceServers : [{
-        urls : "stun:stun.l.google.com:17569"
-    }]
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+	var pc = new RTCPeerConnection({
+		iceServers: [{
+			urls: "stun:stun.l.google.com:17389"
+		}]
+	});
+	for (var i of stream.getVideoTracks()) pc.addTrack(i);
+	var offer = pc.createOffer({
+		offerToReceiveVideo: true,
+		offerToReceiveAudio: true,
+		iceRestart: true
+	});
+	pc.ontrack = (e) => {
+		document.querySelector("video").srcObject = e.streams[0];
+	}
+	pc.onicecandidate = (event) => {
+		if (event.candidate) {
+			pc.setRemoteDescription(event.candidate)
+		}
+	}
 });
-pc.addEventListener("icecandidate",function(e){
-    if(e.candidate){
-        s
-    }
-}
+var pc = new RTCPee
